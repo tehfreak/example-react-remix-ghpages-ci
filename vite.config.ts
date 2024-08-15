52ce464ff9bf91a10b60650d103a2a2f48cbc145
Mickh <tehfreak@tehfreak.com>
2024-08-15 16:22:11 +0300

    chore: define github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const BASE_PATH = process.env.BASE_PATH
+
 export default defineConfig({
+  base: BASE_PATH ? `${BASE_PATH}/` : '/',
   plugins: [
     remix({
       ssr: false,
       appDirectory: "src",
+      basename: BASE_PATH ? `${BASE_PATH}/` : '/',
+      publicPath: BASE_PATH ? `${BASE_PATH}/` : '/',
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
